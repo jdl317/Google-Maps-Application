a52daaf50141efa7c0b763beddcf17c19acb01a3
jefflitterst <jdl317@lehigh.edu>
2016-10-23 17:53:52 -0400

    Added locations and a line between taylor and zoellner

--- app/src/main/java/com/example/jefflitterst/googlemapapp/MapsActivity.java
+++ app/src/main/java/com/example/jefflitterst/googlemapapp/MapsActivity.java
@@ -2,6 +2,7 @@ package com.example.jefflitterst.googlemapapp;
 
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationManager;
@@ -26,6 +27,8 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 
 public class MapsActivity extends AppCompatActivity implements OnMyLocationButtonClickListener, ActivityCompat.OnRequestPermissionsResultCallback, OnMapReadyCallback {
 
@@ -78,6 +81,36 @@ public class MapsActivity extends AppCompatActivity implements OnMyLocationButto
         }
     }
 
+    private void initializeLocations() {
+        LatLng taylorGym = new LatLng(40.607479, -75.374098);
+        LatLng zoellner = new LatLng(40.608167, -75.372677);
+        LatLng universitycenter = new LatLng(40.606141, -75.378222);
+        LatLng campussquare = new LatLng(40.609525, -75.378423);
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(taylorGym));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(taylorGym));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(10));
+        //mMap.addMarker(new MarkerOptions().position(new LatLng(latitude, longitude)).title("You are here!").snippet("Consider yourself located"));
+        mMap.addMarker(new MarkerOptions().position(taylorGym).title("Welcome to Taylor Gym!").snippet("Skies out Thighs out!"));
+        mMap.addMarker(new MarkerOptions().position(zoellner).title("Here's Zoellner!").snippet("So Artistic!"));
+        mMap.addMarker(new MarkerOptions().position(universitycenter).title("The UC!").snippet("Coooool!"));
+        mMap.addMarker(new MarkerOptions().position(campussquare).title("Campus Square!").snippet("Yippee!"));
+        LatLng myCoordinates = taylorGym;//new LatLng(latitude, longitude);
+        CameraUpdate yourLocation = CameraUpdateFactory.newLatLngZoom(myCoordinates, 20);
+        mMap.animateCamera(yourLocation);
+
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(myCoordinates)      // Sets the center of the map to LatLng (refer to previous snippet)
+                .zoom(17)                   // Sets the zoom
+                .bearing(90)                // Sets the orientation of the camera to east
+                .tilt(30)                   // Sets the tilt of the camera to 30 degrees
+                .build();                   // Creates a CameraPosition from the builder
+        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+
+        Polyline line1 = mMap.addPolyline(new PolylineOptions().add(taylorGym, zoellner).width(5).color(Color.RED));
+        return;
+    }
+
     @Override
     public boolean onMyLocationButtonClick() {
         Toast.makeText(this, "My Location button clicked", Toast.LENGTH_SHORT).show();
@@ -86,28 +119,10 @@ public class MapsActivity extends AppCompatActivity implements OnMyLocationButto
         String provider = locationManager.getBestProvider(criteria, true);
         try {
             Location myLocation = locationManager.getLastKnownLocation(provider);
-            double latitude = myLocation.getLatitude();
-            double longitude = myLocation.getLongitude();
-            LatLng latLng = new LatLng(latitude, longitude);
-            LatLng taylorGym = new LatLng(40.607479, -75.374098);
-            LatLng zoellner = new LatLng(40.607267, -75.374077);
-            //mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
-            mMap.moveCamera(CameraUpdateFactory.newLatLng(taylorGym));
-            mMap.animateCamera(CameraUpdateFactory.zoomTo(10));
-            //mMap.addMarker(new MarkerOptions().position(new LatLng(latitude, longitude)).title("You are here!").snippet("Consider yourself located"));
-            mMap.addMarker(new MarkerOptions().position(taylorGym).title("Welcome to Taylor Gym!").snippet("Consider yourself located"));
-            mMap.addMarker(new MarkerOptions().position(zoellner).title("Welcome to Zoellner!").snippet("Consider yourself located"));
-            LatLng myCoordinates = taylorGym;//new LatLng(40.607479, -75.374098);
-            CameraUpdate yourLocation = CameraUpdateFactory.newLatLngZoom(myCoordinates, 10);
-            mMap.animateCamera(yourLocation);
-
-            CameraPosition cameraPosition = new CameraPosition.Builder()
-                    .target(myCoordinates)      // Sets the center of the map to LatLng (refer to previous snippet)
-                    .zoom(17)                   // Sets the zoom
-                    .bearing(90)                // Sets the orientation of the camera to east
-                    .tilt(30)                   // Sets the tilt of the camera to 30 degrees
-                    .build();                   // Creates a CameraPosition from the builder
-            mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+            //double latitude = myLocation.getLatitude();
+            //double longitude = myLocation.getLongitude();
+            //LatLng latLng = new LatLng(latitude, longitude);
+            initializeLocations();
         }catch(SecurityException se){
             Log.d("NO Permissions", "Involving getting last known provider");
         }
@@ -120,8 +135,7 @@ public class MapsActivity extends AppCompatActivity implements OnMyLocationButto
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           String permissions[], int[] grantResults) {
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
         switch (requestCode) {
             case PackageManager.GET_PERMISSIONS: {
                 // If request is cancelled, the result arrays are empty.
